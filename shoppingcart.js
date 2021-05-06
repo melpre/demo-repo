40cfc124013f83448790d5309d1a9b35777da7d0
Melissa Preece <melissanchez@gmail.com>
2021-05-06 16:01:48 -0700

    initial commit

---
+++ shoppingcart.js
@@ -0,0 +1,26 @@
+// My Shopping Cart
+
+// Fruits
+const fruits = ['apple', 'banana', 'cherry', 'grape', 'orange'];
+
+// Snacks
+const snacks = ['doritos', 'oreos', 'snakpak'];
+
+// Vegetables
+const vegetables = ['broccoli', 'carrots', 'peas', 'spinach'];
+
+// Frozen
+const frozen = ['icecream', 'pizza', 'chicken'];
+
+
+// Add new items to Fruits
+fruits.push('mango');
+
+// What's in my shopping cart?
+console.log(fruits);
+console.log(snacks);
+console.log(vegetables);
+console.log(frozen);
+
+
+
